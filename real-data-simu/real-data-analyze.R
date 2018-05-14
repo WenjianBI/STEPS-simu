4f6433dfd2a6aacf7fb5743158615218c4686f78
WenjianBI <38364280+WenjianBI@users.noreply.github.com>
2018-05-14 17:51:11 -0500

    Add files via upload

--- real-data-simu/real-data-analyze.R
+++ real-data-simu/real-data-analyze.R
@@ -3,4 +3,26 @@
 library(devtools)   # author version: 1.13.5
 install_github("WenjianBi/STEPS")
 
-real.data.simu <- read.csv("Y:/SV-evs/final-package/real-data-simu/real-data-simu.csv")
+real.data.simu <- read.csv("STEPS-simu/real-data-simu/real-data-simu.csv")
+
+SNPs=grep("SNP",colnames(real.data.simu),value = T)
+
+Range=range(real.data.simu$Y)
+
+out.steps=c()
+for(i in SNPs){
+  G=real.data.simu[,i]
+  data.mat=with(real.data.simu,cbind(Y,Z,G,Age,Gender))
+  res.tmp=STEPS.snp(data.mat, sec.type="continuous",y0 = Range[1],y3 = Range[2]) 
+  if(class(res.tmp)=="try-error"){
+    out.steps=rbind(out.steps,rep(NA,3));
+  }else{
+    out.steps=rbind(out.steps,
+                    c(res.tmp$pval,res.tmp$fnl.para$res.opt$par[2],res.tmp$inv.fsh[2,2]));
+  }
+}
+out.steps=as.data.frame(out.steps)
+out.steps=cbind(SNPs,out.steps)
+colnames(out.steps)=c("SNP","pval","est-par","est-var")
+
+write.csv("STEPS-simu/real-data-simu/real-data-out.csv",row.names = F)
